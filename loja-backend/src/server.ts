372d05753663c2ebbf704bbb9b1b3058de097c06
Libório Júnior <libajr@materdei.edu.br>
2022-08-09 21:33:23 -0300

    Aula 09/08/2022

--- loja-backend/src/server.ts
+++ loja-backend/src/server.ts
@@ -1,4 +1,6 @@
+import { AppDataSource } from './data-source';
 import express from 'express';
+import cors from 'cors';
 
 //Instancio uma aplicação express
 const app = express();
@@ -6,6 +8,23 @@ const app = express();
 //Determina a porta de execução
 const PORT = 3300;
 
-app.listen(PORT, () => {
-    console.log(`Running in port ${PORT}`);
-})
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Tento conectar ao banco e, se não conseguir, mostro o erro.
+AppDataSource.initialize()
+    .then(() => {
+
+        //Inicio a aplicação
+        app.listen(PORT, () => {
+            console.log(`Running in port ${PORT}`);
+        })
+
+    })
+    .catch(error => {
+        console.log('Ops! Ocorreu um erro.');
+        console.error(error);
+    });
+
+
